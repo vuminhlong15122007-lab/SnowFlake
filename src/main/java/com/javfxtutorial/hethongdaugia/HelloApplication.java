be3f3524da43d6fc0f2230f73f9546aa448b1131
Nguyen Hanh Nguyen <25020297@vnu.edu.vn>
2026-03-24 15:57:18 +0700

    đôổi tên login scene

--- src/main/java/com/javfxtutorial/hethongdaugia/HelloApplication.java
+++ src/main/java/com/javfxtutorial/hethongdaugia/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("màn hình login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login_scene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
