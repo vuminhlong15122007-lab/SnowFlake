03e834cc25dafedaa41832d63b70458c2689de8e
Vu Minh Long <vuminhlong15122007@gmail.com>
2026-03-24 14:09:31 +0700

    update_VML_05 thêm thuộc tính name kiểu String và get/set cho các thuộc tính

--- src/main/java/com/javfxtutorial/hethongdaugia/model/Entity.java
+++ src/main/java/com/javfxtutorial/hethongdaugia/model/Entity.java
@@ -3,6 +3,31 @@ import java.time.LocalDate;
 public abstract class Entity {
     private String id ;
     private LocalDate time ;
+    private String name ;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDate getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDate time) {
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public Entity(String id , LocalDate time ){
         this.id = id ;
