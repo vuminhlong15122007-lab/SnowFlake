62fab79d3e03b62465b4d92a68aa05eb75093d11
Vu Minh Long <vuminhlong15122007@gmail.com>
2026-03-24 14:03:43 +0700

    update_VML_04 chỉnh sửa từ Id thành id trong class Entity

--- src/main/java/com/javfxtutorial/hethongdaugia/model/Entity.java
+++ src/main/java/com/javfxtutorial/hethongdaugia/model/Entity.java
@@ -1,11 +1,11 @@
 package com.javfxtutorial.hethongdaugia.model;
 import java.time.LocalDate;
 public abstract class Entity {
-    private String Id ;
+    private String id ;
     private LocalDate time ;
 
-    public Entity(String Id , LocalDate time ){
-        this.Id = Id ;
+    public Entity(String id , LocalDate time ){
+        this.id = id ;
         this.time = time ;
     }
 
