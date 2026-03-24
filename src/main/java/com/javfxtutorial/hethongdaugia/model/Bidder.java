af2114648b69eaed3118ab09cbbba873578ac0ee
Vu Minh Long <vuminhlong15122007@gmail.com>
2026-03-24 15:23:28 +0700

    update_013 : Bidder - thêm method requestToBid , upDateMoney , registerSeller

--- src/main/java/com/javfxtutorial/hethongdaugia/model/Bidder.java
+++ src/main/java/com/javfxtutorial/hethongdaugia/model/Bidder.java
@@ -1,15 +1,15 @@
 package com.javfxtutorial.hethongdaugia.model;
 
 public class Bidder extends User {
-    private double Money;
+    private double money;
     private int counterID = 0;
 
     public double getMoney() {
-        return Money;
+        return money;
     }
 
     public void setMoney(double money) {
-        Money = money;
+        money = money;
     }
 
     public int getCounterID() {
@@ -20,11 +20,30 @@ public class Bidder extends User {
         this.counterID = counterID;
     }
 
+
     public String Generate_Id(){
-        this.setCounterID(this.getCounterID() + 1);
-        return "BD" + String.format("%03d", this.getCounterID());
+        this.counterID = counterID + 1;
+        return "BD" + String.format("%03d", counterID);
     }
-    public Bidder(String id , String name ; String passWord ; String email ; String fullName ; double money){
-        supper()
+    public Bidder(String name , String passWord , String email , String fullName , double money){
+        super(name , passWord , email , fullName);
+        this.money = money ;
     }
+    public void registerSeller(){} // chưa nghĩ ra
+    public void upDateMoney(double money1){
+        this.money = money1;
+    }
+    public void requestToBid(Auction ac ,double amount){
+        if (this.money >= amount){
+            System.out.println(this.getName() + " đang gửi yêu cầu đấu giá");
+            boolean success = ac.placeBid(this , amount);
+            if (success){
+                //tạm thời trừ tiền
+                // this.money -= amount
+            }else{
+                System.out.println(this.getName() + " không đủ tiền trong tài khoản");
+            }
+        }
+    }
+
 }
