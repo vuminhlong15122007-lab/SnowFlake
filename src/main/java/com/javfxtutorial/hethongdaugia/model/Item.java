6bc6ea54609f889424bac64e56615918a06bc20d
DAnh <25020017@vnu.edu.vn>
2026-03-24 15:30:47 +0700

    "Update Item"

--- src/main/java/com/javfxtutorial/hethongdaugia/model/Item.java
+++ src/main/java/com/javfxtutorial/hethongdaugia/model/Item.java
@@ -1,6 +1,17 @@
 package com.javfxtutorial.hethongdaugia.model;
+import java.time.LocalDate;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class Item {
-    double initPrice, highestPrice, stepPrice;
+public abstract class Item {
+    private double initPrice, highestPrice, stepPrice;
+    private Seller idSeller;
+    private String idItem;
+    private LocalDate startTime;
+    private LocalDate endTime;
+    public String description;
+    private String imagePath; // đường dẫn ảnh
+
+
 
 }
