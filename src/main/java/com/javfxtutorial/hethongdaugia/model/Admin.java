3c512655c3f3b012ba10fa59d865f3845d8888d1
DAnh <25020017@vnu.edu.vn>
2026-03-24 14:58:38 +0700

    "Update Admin"

--- src/main/java/com/javfxtutorial/hethongdaugia/model/Admin.java
+++ src/main/java/com/javfxtutorial/hethongdaugia/model/Admin.java
@@ -1,6 +1,12 @@
 package com.javfxtutorial.hethongdaugia.model;
 
 public class Admin extends User {
+    private double counter = 0;
+
+    public Admin(String id, String name, String passWord, String fullName, String email, double counter) {
+        super(id, name, passWord, fullName, email);
+        this.counter = counter;
+    }
 
     public void banUser(){
 
@@ -14,4 +20,9 @@ public class Admin extends User {
 
     }
 
+    @Override
+    public String Generate_Id(){
+        counter ++;
+        return "AD " + counter + String.format("%03d", counter);
+    }
 }
