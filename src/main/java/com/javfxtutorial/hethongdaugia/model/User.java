7d0761d3bfe5fa7b3d38e2cc85671742150e4de5
Vu Minh Long <vuminhlong15122007@gmail.com>
2026-03-24 14:46:26 +0700

    09 sửa lỗi khởi tạo của User

--- src/main/java/com/javfxtutorial/hethongdaugia/model/User.java
+++ src/main/java/com/javfxtutorial/hethongdaugia/model/User.java
@@ -7,8 +7,8 @@ public abstract class User extends Entity {
     private String email;
     private String fullName;
 
-    public User(String id, LocalDate time, String passWord, String fullName, String email) {
-        super(id, time);
+    public User(String id, String name, String passWord, String fullName, String email) {
+        super(id, name);
         this.passWord = passWord;
         this.fullName = fullName;
         this.email = email;
