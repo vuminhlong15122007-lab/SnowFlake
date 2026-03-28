0c9a4d983e9c9dccce324462e58038b6a00ed783
Nguyen Hanh Nguyen <25020297@vnu.edu.vn>
2026-03-28 12:19:04 +0700

    thêm dependency sql

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.javfxtutorial.hethongdaugia {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires mysql.connector.j;
 
 
     opens com.javfxtutorial.hethongdaugia to javafx.fxml;
