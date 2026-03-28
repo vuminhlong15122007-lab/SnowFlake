eaea66f53be18f7977fe4a5c1da1587299dd25d5
Nguyen Hanh Nguyen <25020297@vnu.edu.vn>
2026-03-28 17:12:53 +0700

    chinhrh lại đường dẫn

--- src/main/java/com/javfxtutorial/hethongdaugia/MainApplication.java
+++ src/main/java/com/javfxtutorial/hethongdaugia/MainApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("view/login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("SnowFox");
         stage.setScene(scene);
