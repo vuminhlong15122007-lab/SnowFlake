a352af90d677ef6c0f3d298c72d127ae44465e4a
Nguyen Hanh Nguyen <nguyenhanh0510.tt@gmail.com>
2026-04-04 11:33:27 +0700

    cònlict

--- src/main/java/com/javfxtutorial/hethongdaugia/client/controller/TrangChuController.java
+++ src/main/java/com/javfxtutorial/hethongdaugia/client/controller/TrangChuController.java
@@ -1,4 +1,26 @@
 package com.javfxtutorial.hethongdaugia.client.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class TrangChuController {
+    //truy cap thong tin nguoi dung
+    @FXML
+    private Button profileButton;
+    public void goToProfile(ActionEvent event){
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("man_hinh_hien_thong_tin_User.fxml"));
+            Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
