118a0e1c026ea59422b3e188744a297509010378
Nguyen Hanh Nguyen <nguyenhanh0510.tt@gmail.com>
2026-04-11 17:34:35 +0700

    logic đặt giá + xử lý đặt giá đồng thời + thêm currentAuction ở Model

--- src/main/java/com/javfxtutorial/hethongdaugia/server/manager/AuctionManger.java
+++ src/main/java/com/javfxtutorial/hethongdaugia/server/manager/AuctionManger.java
@@ -1,4 +1,40 @@
 package com.javfxtutorial.hethongdaugia.server.manager;
 
+import com.javfxtutorial.hethongdaugia.common.model.Auction;
+import com.javfxtutorial.hethongdaugia.server.dao.AuctionDAO;
+
 public class AuctionManger {
+
+    private static AuctionManger instance;
+    private AuctionManger(){}
+    public static AuctionManger getInstance(){
+        if (instance == null){
+            instance = new AuctionManger();
+        }
+        return instance;
+    }
+
+    Auction currentAuction;
+    public void setCurrentAuction(Auction currentAuction) {
+        this.currentAuction = currentAuction;
+    }
+
+    public boolean checkValidBid(double amount){
+        double currentPrice = AuctionDAO.getInstance().selectById(currentAuction.getAuctionId()).getCurrentPrice(); //vào DAO check giá hiện tại
+        if (amount < currentPrice + currentAuction.getStepPrice()){
+            return true;
+        }
+        System.out.println("Cần đặt giá cao hơn giá hiện tại + bước giá");
+        return false;
+    }
+
+    public synchronized boolean placeBid(double amount){
+        if (checkValidBid(amount)) {
+            currentAuction = AuctionDAO.getInstance().selectById(currentAuction.getAuctionId());
+            currentAuction.setCurrentPrice(amount);
+            AuctionDAO.getInstance().update(currentAuction);
+            return true;
+        }
+        return false;
+    }
 }
