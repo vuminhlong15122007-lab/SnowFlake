cff7793427c9b66117cb7527ff0692f40437e11e
Vu Minh Long <vuminhlong15122007@gmail.com>
2026-04-11 21:55:03 +0700

    Update_0.16.17 : cập nhật database lên online

---
+++ src/main/java/com/javfxtutorial/hethongdaugia/client/controller/Edit_User_Popup_Controller.java
@@ -0,0 +1,4 @@
+package com.javfxtutorial.hethongdaugia.client.controller;
+
+public class Edit_User_Popup_Controller {
+}
--- src/main/java/com/javfxtutorial/hethongdaugia/server/dao/JDBCUtil.java
+++ src/main/java/com/javfxtutorial/hethongdaugia/server/dao/JDBCUtil.java
@@ -1,7 +1,5 @@
 package com.javfxtutorial.hethongdaugia.server.dao;
 
-import com.mysql.cj.jdbc.Driver;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,32 +7,40 @@ import java.sql.SQLException;
 public class JDBCUtil {
     public static Connection getConnection() {
         Connection connection = null;
-
         try {
-// bước 1: đăng kí MySQL driver với lớp DriverManger
-            com.mysql.cj.jdbc.Driver driver = new Driver();//tạo driver
-            DriverManager.registerDriver(driver);
-// bước 2: khai báo các thông số cơ bản yêu cầu cho kết nối
-            String url = "jdbc:mysql://localhost:3306/he_thong_dau_gia";
-            String username = "root";
-            String password = "123456";
-// bước 3: tạo kết nối
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            String host = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com";
+            String port = "4000";
+            // Cẩn thận chỗ này: nếu bạn đã tạo DB riêng trên HeidiSQL thì sửa lại tên
+            String database = "test";
+            String username = "3sSzrSFdZfqFKd5.root";
+            String password = "8J1D7oKnbj8npKF1";
+
+            // Chuỗi URL chuẩn cho TiDB Cloud
+            String url = "jdbc:mysql://" + host + ":" + port + "/" + database +
+                    "?enabledTLSProtocols=TLSv1.2,TLSv1.3&sslMode=REQUIRED"; // Đổi thành REQUIRED cho "nhẹ"
+
             connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Kết nối thành công tới TiDB Cloud!");
+
+        } catch (ClassNotFoundException e) {
+            System.err.println("Lỗi: Thiếu Driver MySQL!");
+            e.printStackTrace();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            System.err.println("Lỗi kết nối: " + e.getMessage());
+            // Nếu lỗi "Access denied for IP", hãy kiểm tra lại IP Access List trên web TiDB
         }
         return connection;
     }
 
     public static void closeConnection(Connection connection){
         try{
-            if (connection != null){
+            if (connection != null && !connection.isClosed()){
                 connection.close();
             }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
-
-
 }
