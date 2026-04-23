d681c5e536893548622d4d87b8baeada3819d3e4
Nguyen Hanh Nguyen <nguyenhanh0510.tt@gmail.com>
2026-04-23 07:48:04 +0700

    thêm synchronized

--- src/main/java/com/javfxtutorial/hethongdaugia/client/network/ServerConnection.java
+++ src/main/java/com/javfxtutorial/hethongdaugia/client/network/ServerConnection.java
@@ -46,7 +46,7 @@ public class ServerConnection {
         }
     }
 
-    public Response receiveResponse() throws IOException, ClassNotFoundException {
+    public synchronized Response receiveResponse() throws IOException, ClassNotFoundException {
         return (Response) in.readObject();
     }
     public void close() throws IOException {
