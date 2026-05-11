b0767b13e45293b00d986461e8494fdc5bd2b40b
DAnh <25020017@vnu.edu.vn>
2026-05-11 13:41:32 +0700

    DA.03.06: SP khác

--- src/main/java/com/javfxtutorial/hethongdaugia/common/model/factory/ItemFactory.java
+++ src/main/java/com/javfxtutorial/hethongdaugia/common/model/factory/ItemFactory.java
@@ -1,8 +1,19 @@
 package com.javfxtutorial.hethongdaugia.common.model.factory;
 
 import com.javfxtutorial.hethongdaugia.common.model.Item;
+import com.javfxtutorial.hethongdaugia.common.model.enums.ItemCategory;
+import com.javfxtutorial.hethongdaugia.server.factory.ArtDAOFactory;
+import com.javfxtutorial.hethongdaugia.server.factory.DefaultItemDAOFactory;
+import com.javfxtutorial.hethongdaugia.server.factory.ElectronicsDAOFactory;
+import com.javfxtutorial.hethongdaugia.server.factory.VehicleDAOFactory;
 
 public abstract class ItemFactory {
     public abstract Item createItem();
-    
+    public static ItemFactory getFactory(ItemCategory category){
+        if(category == ItemCategory.Vehicle) { return new VehicleFactory(); }
+        else if (category == ItemCategory.Art) { return new ArtFactory(); }
+        else if (category == ItemCategory.Electronics) { return new ElectronicsFactory(); }
+        return new DefaultItemFactory();
+
+    }
 }
