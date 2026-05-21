e6992f649cb74859fcf02e8819e36a282a712596
Nguyen Hong Lan <nguyenhlan027@gmail.com>
2026-05-21 15:04:53 +0700

    L16.fix: fix admin

--- src/main/java/com/javfxtutorial/hethongdaugia/server/manager/AuctionManager.java
+++ src/main/java/com/javfxtutorial/hethongdaugia/server/manager/AuctionManager.java
@@ -225,6 +225,9 @@ public class AuctionManager {
     public AuctionStatus refreshAuctionStatus(Auction auction) throws DataException {
         AuctionStatus previousStatus = auction.getStatus();
 
+        if (previousStatus == AuctionStatus.CANCELLED) {
+            return previousStatus;
+        }
         if (LocalDateTime.now().isBefore(auction.getStartingTime())) {
             auction.setStatus(AuctionStatus.NOT_START);
         } else if (previousStatus == AuctionStatus.PAID) {
