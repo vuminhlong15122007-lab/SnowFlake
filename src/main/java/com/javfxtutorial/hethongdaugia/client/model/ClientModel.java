acbe72aef752a161f07543e4f2a250b2874975ab
DAnh <25020017@vnu.edu.vn>
2026-05-29 12:32:37 +0700

    Fix 10.11 : sua Canhr caos cho no hien 1 lan xong danh sau vao disk roi khong hien lai nua

--- src/main/java/com/javfxtutorial/hethongdaugia/client/model/ClientModel.java
+++ src/main/java/com/javfxtutorial/hethongdaugia/client/model/ClientModel.java
@@ -154,6 +154,7 @@ public class ClientModel {
     return readNotificationIds.containsKey(auctionId);
   }
 
+
   /**
    * Đánh dấu notification của 1 auction là đã đọc. Lưu cả thời điểm đọc để sau 2 ngày có thể tự
    * động xóa.
@@ -229,6 +230,16 @@ public class ClientModel {
     if (pruneScheduler != null && !pruneScheduler.isShutdown()) {
       pruneScheduler.shutdown();
     }
+  }
+
+  public boolean isLawsuitWarned(int userID, int auctionID ){
+    Preferences preferences = Preferences.userRoot().node(userID + "/lawsuit_warned");
+    return preferences.getBoolean("auction:"+ auctionID , false);
+  }
+
+  public void markLawsuitWarned(int userID, int auctionID) {
+    Preferences preferences = Preferences.userRoot().node(userID + "/lawsuit_warned");
+    preferences.putBoolean("auction:"+ auctionID , true);
   }
 
   public void logout() {
